dc022034e979baa9a8a1b276edda032739ad7998
Shea Cronin <shealcronin@gmail.com>
2021-01-05 18:42:40 -0500

    Make form submission add a book to library

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 let myLibrary = ['Harry Potter', 'Normal People'];
 
-function Book() {
-
+// Constructor of book objects.
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary() {
-
-}
-
+// Create form variables.
 const newBookForm = document.querySelector('form');
 let isFormVisible = false;
+
+// Creates new book object and pushes to library array.
+function addBookToLibrary(e) {
+    const title = document.querySelector('#title');
+    const author = document.querySelector('#author');
+    const pages = document.querySelector('#pages');
+    const read = document.querySelector('#read');
+
+    let book = new Book(title.value, author.value, pages.value, read.value);
+    myLibrary.push(book);
+    console.log(myLibrary);
+
+    // Update display.
+    displayBooks();
+}
 
 function toggleDisplayForm() {
     if (!isFormVisible) {
@@ -23,6 +39,10 @@ function toggleDisplayForm() {
 
 const newBookBtn = document.querySelector('#new-book-btn');
 newBookBtn.addEventListener('click', toggleDisplayForm);
+
+// Button to submit new book form.
+const submitBtn = document.querySelector('#submit-btn');
+submitBtn.addEventListener('click', addBookToLibrary);
 
 const shelf = document.querySelector('#shelf');
 
