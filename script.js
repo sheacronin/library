4f24fa3a5fb1dadb853e4903bf8e3f1d7ab19b70
Shea Cronin <shealcronin@gmail.com>
2021-01-05 16:51:00 -0500

    Add function to display books in library

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+let myLibrary = ['Harry Potter', 'Normal People'];
 
 function Book() {
 
@@ -7,3 +7,15 @@ function Book() {
 function addBookToLibrary() {
 
 }
+
+const shelf = document.querySelector('#shelf');
+
+function displayBooks() {
+    for (book of myLibrary) {
+        let bookCard = document.createElement('div');
+        bookCard.textContent = book;
+        shelf.appendChild(bookCard);
+    }
+}
+
+displayBooks();
